8c2434683821c58a186e8c299a725e72d7d81399
klarahlmattsson-eng <klara.hlmattsson@gmail.com>
2026-03-22 16:03:33 +0100

    Delete supabase.ts

--- supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://mieekqibjzgiqvddiyrl.supabase.co";
-const supabaseAnonKey = "sb_publishable_Ti7uaDcfR7oatEO73bbjkA_6YQr5ef0";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
