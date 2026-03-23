aa50432f1c14cfc93ad43aae626b6c70548af577
klarahlmattsson-eng <klara.hlmattsson@gmail.com>
2026-03-23 09:39:28 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
+import { supabase } from "../lib/supabase";
+
+export default async function Home() {
+  const { data: teams } = await supabase
+    .from("teams")
+    .select("*")
+    .order("points", { ascending: false });
+
   return (
     <main style={{ padding: 20 }}>
       <h1>Poängtavla</h1>
-      <p>Detta funkar nu 🎉</p>
+
+      {teams?.map((team) => (
+        <div key={team.id}>
+          {team.name} - {team.points} poäng
+        </div>
+      ))}
     </main>
   );
 }
